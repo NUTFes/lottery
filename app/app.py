28cf85e937081f69924f329157d0ace1d6206599
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-08-03 19:18:21 +0900

    flask hello world

---
+++ app/app.py
@@ -0,0 +1,22 @@
+#Flaskとrender_template（HTMLを表示させるための関数）をインポート
+from flask import Flask,render_template
+
+#Flaskオブジェクトの生成
+app = Flask(__name__)
+
+
+#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+#「/index」へアクセスがあった場合に、「index.html」を返す
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+#おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
