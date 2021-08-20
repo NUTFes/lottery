fd0eb9c5e116964cbaaf9b10b385bb4c216a6768
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-08-20 16:13:28 +0900

    [mod] Fast-API tutorial day3 (#10)

--- Fast-API/urls.py
+++ Fast-API/urls.py
@@ -3,4 +3,5 @@ from controllers import *
 
 # FastAPIのルーティング用関数
 app.add_api_route('/', index)
-app.add_api_route('/admin', admin)  # new
+app.add_api_route('/admin', admin)
+app.add_api_route('/register', register, methods=['GET', 'POST'])
