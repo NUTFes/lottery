5e951c2434c8e050eb46fb1598d11cf167e9b3f2
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-08-22 01:42:11 +0900

    [wip] day5 (#10)

--- Fast-API/controllers.py
+++ Fast-API/controllers.py
@@ -15,6 +15,12 @@ pattern = re.compile(r'\w{4,20}')
 pattern_pw = re.compile(r'\w{6,20}')
 pattern_mail = re.compile(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')
 
+from datetime import datetime
+from datetime import timedelta
+
+from auth import auth
+
+from starlette.responses import RedirectResponse
 
 app = FastAPI(
   title='FastAPIでつくるtoDoアプリケーション',
@@ -31,15 +37,19 @@ def index(request: Request):
   return templates.TemplateResponse('index.html', 
                                    {'request': request})
 
+
 def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
-    # Basic認証で受け取った情報
-    username = credentials.username
+
+    username = auth(credentials)  
     password = hashlib.md5(credentials.password.encode()).hexdigest()
- 
-    # データベースからユーザ名が一致するデータを取得
+
     user = db.session.query(User).filter(User.username == username).first()
-    task = db.session.query(Task).filter(Task.user_id == user.id).all() if user is not None else []
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
     db.session.close()
+
+    # 今日の日付と来週の日付
+    today = datetime.now()
+    next_w = today + timedelta(days=7)  # １週間後の日付
  
     # 該当ユーザがいない場合
     if user is None or user.password != password:
@@ -105,3 +115,101 @@ async def register(request: Request):
     return templates.TemplateResponse('complete.html',
                                       {'request': request,
                                         'username': username})
+
+  # controllers.py
+def detail(request: Request, username, year, month, day,
+           credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証OK？
+    username_tmp = auth(credentials)
+
+    if username_tmp != username:  # もし他のユーザが訪問してきたらはじく
+        return RedirectResponse('/')
+
+    # ログインユーザを取得
+    user = db.session.query(User).filter(User.username == username).first()
+    # ログインユーザのタスクを取得
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+
+    # 該当の日付と一致するものだけのリストにする
+    theday = '{}{}{}'.format(year, month.zfill(2), day.zfill(2))  # 月日は0埋めする
+    task = [t for t in task if t.deadline.strftime('%Y%m%d') == theday]
+
+    return templates.TemplateResponse('detail.html',
+                                      {'request': request,
+                                       'username': username,
+                                       'task': task,  # new
+                                       'year': year,
+                                       'month': month,
+                                       'day': day})
+
+async def done(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証OK？
+    username = auth(credentials)
+ 
+    # ユーザ情報を取得
+    user = db.session.query(User).filter(User.username == username).first()
+ 
+    # ログインユーザのタスクを取得
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+ 
+    # フォームで受け取ったタスクの終了判定を見て内容を変更する
+    data = await request.form()
+    t_dones = data.getlist('done[]')  # リストとして取得
+ 
+    for t in task:
+        if str(t.id) in t_dones:  # もしIDが一致すれば "終了した予定" とする
+            t.done = True
+ 
+    db.session.commit()  # update!!
+    db.session.close()
+ 
+    return RedirectResponse('/admin')  # 管理者トップへリダイレクト
+    
+
+async def add(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証
+    username = auth(credentials)
+
+    # ユーザ情報を取得
+    user = db.session.query(User).filter(User.username == username).first()
+
+    # フォームからデータを取得
+    data = await request.form()
+    year = int(data['year'])
+    month = int(data['month'])
+    day = int(data['day'])
+    hour = int(data['hour'])
+    minute = int(data['minute'])
+
+    deadline = datetime(year=year, month=month, day=day,
+                        hour=hour, minute=minute)
+
+    # 新しくタスクを生成しコミット
+    task = Task(user.id, data['content'], deadline)
+    db.session.add(task)
+    db.session.commit()
+    db.session.close()
+
+    return RedirectResponse('/admin')
+
+def delete(request: Request, t_id, credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証
+    username = auth(credentials)
+ 
+    # ログインユーザ情報を取得
+    user = db.session.query(User).filter(User.username == username).first()
+ 
+    # 該当タスクを取得
+    task = db.session.query(Task).filter(Task.id == t_id).first()
+ 
+    # もしユーザIDが異なれば削除せずリダイレクト
+    if task.user_id != user.id:
+        return RedirectResponse('/admin')
+ 
+    # 削除してコミット
+    db.session.delete(task)
+    db.session.commit()
+    db.session.close()
+ 
+    return RedirectResponse('/admin')
