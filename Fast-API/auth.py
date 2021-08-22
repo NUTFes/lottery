2a953dd193882e474d468a8ff1b7bb7b52b5d4b1
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-08-23 02:58:22 +0900

    [mod] Fast-API (#12)

--- Fast-API/auth.py
+++ Fast-API/auth.py
@@ -1,6 +1,6 @@
 import hashlib
 import db
-from models import User
+from models import Place
 from starlette.status import HTTP_401_UNAUTHORIZED
 from fastapi import HTTPException
 
@@ -8,18 +8,18 @@ from fastapi import HTTPException
 def auth(credentials):
     """ Basic認証チェック """
     # Basic認証で受け取った情報
-    username = credentials.username
+    placename = credentials.username
     password = hashlib.md5(credentials.password.encode()).hexdigest()
     # データベースからユーザ名が一致するデータを取得
-    user = db.session.query(User).filter(User.username == username).first()
+    place = db.session.query(Place).filter(Place.placename == placename).first()
     db.session.close()
 
     # 該当ユーザがいない場合
-    if user is None or user.password != password:
+    if place is None or place.password != password:
         error = 'ユーザ名かパスワードが間違っています．'
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail=error,
             headers={"WWW-Authenticate": "Basic"},
         )
-    return username
+    return placename
--- Fast-API/controllers.py
+++ Fast-API/controllers.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-from starlette.templating import Jinja2Templates  # new
+from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 import db
-from models import User, Task
+from models import Place, Log
 
 import hashlib
 
 import re
 pattern = re.compile(r'\w{4,20}')
 pattern_pw = re.compile(r'\w{6,20}')
-pattern_mail = re.compile(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')
 
 from datetime import datetime
 from datetime import timedelta
@@ -40,11 +39,11 @@ def index(request: Request):
 
 def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
 
-    username = auth(credentials)  
+    placename = auth(credentials)
     password = hashlib.md5(credentials.password.encode()).hexdigest()
 
-    user = db.session.query(User).filter(User.username == username).first()
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    place = db.session.query(Place).filter(Place.placename == placename).first()
+    log = db.session.query(Log).filter(Log.place_id == place.id).all()
     db.session.close()
 
     # 今日の日付と来週の日付
@@ -52,7 +51,7 @@ def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security
     next_w = today + timedelta(days=7)  # １週間後の日付
  
     # 該当ユーザがいない場合
-    if user is None or user.password != password:
+    if place is None or place.password != password:
         error = 'ユーザ名かパスワードが間違っています'
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
@@ -63,177 +62,104 @@ def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security
     # 特に問題がなければ管理者ページへ
     return templates.TemplateResponse('admin.html',
                                       {'request': request,
-                                       'user': user,
-                                       'task': task})
+                                       'place': place,
+                                       'log': log})
 
-# controllers.py
+
 async def register(request: Request):
   if request.method == 'GET':
     return templates.TemplateResponse('register.html',
                                       {'request': request,
-                                        'username': '',
+                                        'placename': '',
                                         'error': []})
 
   if request.method == 'POST':
     # POSTデータ
     data = await request.form()
-    username = data.get('username')
+    placename = data.get('placename')
     password = data.get('password')
     password_tmp = data.get('password_tmp')
-    mail = data.get('mail')
 
-    # new ここから
     error = []
 
-    tmp_user = db.session.query(User).filter(User.username == username).first()
+    tmp_place = db.session.query(Place).filter(Place.placename == placename).first()
 
     # 怒涛のエラー処理
-    if tmp_user is not None:
-      error.append('同じユーザ名のユーザが存在します。')
+    if tmp_place is not None:
+      error.append('同じ場所が存在します。')
     if password != password_tmp:
       error.append('入力したパスワードが一致しません。')
-    if pattern.match(username) is None:
-      error.append('ユーザ名は4~20文字の半角英数字にしてください。')
+    # if pattern.match(placename) is None:
+    #   error.append('ユーザ名は4~20文字の半角英数字にしてください。')
     if pattern_pw.match(password) is None:
       error.append('パスワードは6~20文字の半角英数字にしてください。')
-    if pattern_mail.match(mail) is None:
-      error.append('正しくメールアドレスを入力してください。')
-
     # エラーがあれば登録ページへ戻す
     if error:
       return templates.TemplateResponse('register.html',
                                         {'request': request,
-                                          'username': username,
-                                          'error': error})
-
+                                         'placename': placename,
+                                         'error': error})
     # 問題がなければユーザ登録
-    user = User(username, password, mail)
-    db.session.add(user)
+    place = Place(placename, password)
+    db.session.add(place)
     db.session.commit()
     db.session.close()
 
     return templates.TemplateResponse('complete.html',
-                                      {'request': request,
-                                        'username': username})
+                                     {'request': request,
+                                      'placename': placename})
 
-  # controllers.py
-def detail(request: Request, username, year, month, day,
-           credentials: HTTPBasicCredentials = Depends(security)):
-    # 認証OK？
-    username_tmp = auth(credentials)
-
-    if username_tmp != username:  # もし他のユーザが訪問してきたらはじく
-        return RedirectResponse('/')
-
-    # ログインユーザを取得
-    user = db.session.query(User).filter(User.username == username).first()
-    # ログインユーザのタスクを取得
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
-    db.session.close()
-
-    # 該当の日付と一致するものだけのリストにする
-    theday = '{}{}{}'.format(year, month.zfill(2), day.zfill(2))  # 月日は0埋めする
-    task = [t for t in task if t.deadline.strftime('%Y%m%d') == theday]
-
-    return templates.TemplateResponse('detail.html',
-                                      {'request': request,
-                                       'username': username,
-                                       'task': task,  # new
-                                       'year': year,
-                                       'month': month,
-                                       'day': day})
-
-async def done(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
-    # 認証OK？
-    username = auth(credentials)
- 
-    # ユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
- 
-    # ログインユーザのタスクを取得
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
- 
-    # フォームで受け取ったタスクの終了判定を見て内容を変更する
-    data = await request.form()
-    t_dones = data.getlist('done[]')  # リストとして取得
- 
-    for t in task:
-        if str(t.id) in t_dones:  # もしIDが一致すれば "終了した予定" とする
-            t.done = True
- 
-    db.session.commit()  # update!!
-    db.session.close()
- 
-    return RedirectResponse('/admin')  # 管理者トップへリダイレクト
-    
 
 async def add(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
     # 認証
-    username = auth(credentials)
-
+    placename = auth(credentials)
     # ユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
-
-    # フォームからデータを取得
+    place = db.session.query(Place).filter(Place.placename == placename).first()
+    # # フォームからデータを取得
     data = await request.form()
-    year = int(data['year'])
-    month = int(data['month'])
-    day = int(data['day'])
-    hour = int(data['hour'])
-    minute = int(data['minute'])
-
-    deadline = datetime(year=year, month=month, day=day,
-                        hour=hour, minute=minute)
-
-    # 新しくタスクを生成しコミット
-    task = Task(user.id, data['content'], deadline)
-    db.session.add(task)
+    student_id = int(data['student_id'])
+    log = Log(place.id, student_id)
+    db.session.add(log)
     db.session.commit()
     db.session.close()
 
     return RedirectResponse('/admin')
 
-def delete(request: Request, t_id, credentials: HTTPBasicCredentials = Depends(security)):
+
+def delete(t_id, credentials: HTTPBasicCredentials = Depends(security)):
     # 認証
-    username = auth(credentials)
- 
+    placename = auth(credentials)
     # ログインユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
- 
+    place = db.session.query(Place).filter(Place.placename == placename).first()
     # 該当タスクを取得
-    task = db.session.query(Task).filter(Task.id == t_id).first()
- 
+    log = db.session.query(Log).filter(Log.id == t_id).first()
     # もしユーザIDが異なれば削除せずリダイレクト
-    if task.user_id != user.id:
+    if log.place_id != place.id:
         return RedirectResponse('/admin')
- 
     # 削除してコミット
-    db.session.delete(task)
+    db.session.delete(log)
     db.session.commit()
     db.session.close()
- 
+    
     return RedirectResponse('/admin')
 
 
-def get(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+def get(credentials: HTTPBasicCredentials = Depends(security)):
     # 認証
-    username = auth(credentials)
- 
+    placename = auth(credentials)
     # ユーザ情報を取得
-    user = db.session.query(User).filter(User.username == username).first()
- 
+    place = db.session.query(Place).filter(Place.placename == placename).first()
     # タスクを取得
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
- 
+    log = db.session.query(Log).filter(Log.place_id == place.id).all()
+
     db.session.close()
- 
+
     # JSONフォーマット
-    task = [{
+    log = [{
         'id': t.id,
-        'content': t.content,
-        'deadline': t.deadline.strftime('%Y-%m-%d %H:%M:%S'),
-        'published': t.date.strftime('%Y-%m-%d %H:%M:%S'),
-        'done': t.done,
-    } for t in task]
- 
-    return task
+        'place_id': t.place_id,
+        'student_id': t.student_id,
+        'time': t.time.strftime('%Y-%m-%d %H:%M:%S'),
+    } for t in log]
+
+    return log
