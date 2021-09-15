478df89fbb9c5674c70f41219bdb1ed6b5066d32
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-09-16 06:02:09 +0900

    [wip](#59)

--- nfc-reader/readic.py
+++ nfc-reader/readic.py
@@ -15,60 +15,53 @@ REQUEST_URL = "http://localhost:8000"
 POST_NUMBER_URI = REQUEST_URL +'/api/place/'+ str(REQUEST_PLACE_ID) +'/add'
 POST_MESSAGE_URI = REQUEST_URL +'/api/place/'+ str(REQUEST_PLACE_ID) +'/message/add'
 
-
-global id
-global oldres
-oldres = {
+global res
+global tag
+res = {
     'number': 00000000, 
     'expiration': 00000000, 
     'updated_at': dt_now}
 
-def connected(tag):
-    global newres
-
-    idm, pmm = tag.polling(system_code=0xfe00)
-    tag.idm, tag.pmm, tag.sys = idm, pmm, 0xfe00
-
-    service_code = [nfc.tag.tt3.ServiceCode(0x1A8B >> 6, 0x1A8B & 0x3f)]
-    bc_id = [nfc.tag.tt3.BlockCode(0)]
-    id = tag.read_without_encryption(service_code, bc_id) # 学籍番号
-
-    newres = {
-        'number': id[2:10].decode("utf-8"), 
-        'expiration': id[2:10].decode("utf-8"), 
-        'updated_at': dt_now }
-        
+def connected(): #tag       
     return False
 
-def is_enable_register():
+def is_enable_register(res):
     try:
-        if newres.number == oldres.number and (newres.updated_at - oldres.updated_at).seconds > 5*60:
+        if res.number == res.number and (res.updated_at - res.updated_at).seconds > 5*60:
             raise Exception
-        if datetime.datetime.strptime(str(newres.expiration),'%Y%m%d') > oldres.updated_at:
+        if datetime.datetime.strptime(str(res.expiration),'%Y%m%d') > res.updated_at:
             raise Exception
-        post_number(newres.number)
-        post_message(str(newres.number))
+        post_number(res.number)
+        post_message(str(res.number))
 
     except:
         return False
+
+def touch_card(clf):
+    data = clf.connect(rdwr={'on-connect': connected})#NFCリーダーを起動して,NFCリーダーのタッチ検出をします
+    idm, pmm = data.polling(system_code=0xfe00)
+    data.idm, data.pmm, data.sys = idm, pmm, 0xfe00
+    service_code = [nfc.tag.tt3.ServiceCode(0x1A8B >> 6, 0x1A8B & 0x3f)]
+    bc_id = [nfc.tag.tt3.BlockCode(0)]
+    id = data.read_without_encryption(service_code, bc_id) # 学籍番号
+    newres = {
+        'number': id[2:10].decode("utf-8"), 
+        'expiration': id[2:10].decode("utf-8"), 
+        'updated_at': dt_now
+        }
+    return newres
+
 
 def scan_card():
     try:
         clf = nfc.ContactlessFrontend('usb') #USB接続のNFCリーダーを開く
         try:
-            clf.connect(rdwr={'on-connect': connected})#NFCリーダーを起動して,NFCリーダーのタッチ検出をします
+            res = touch_card(clf)
             clf.close()
-            is_enable_register()
+            is_enable_register(res)
 
-            
-        except nfc.tag.tt3.Type1TagCommandError:
-            clf.close()
-        except nfc.tag.tt3.Type2TagCommandError:
-            clf.close()
         except nfc.tag.tt3.Type3TagCommandError:
             post_message("タッチが短すぎます")
-            clf.close()
-        except nfc.tag.tt3.Type4TagCommandError:
             clf.close()
         except nfc.tag.TagCommandError as e:
             if e.errno > 0x00FF:
