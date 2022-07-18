7c2efd2375546fd66c54d1b843ed2950a9d83666
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-07-18 21:05:00 +0900

    [mod] mobile layout components (#117)

--- next/src/pages/admin.tsx
+++ next/src/pages/admin.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
-import MobileLayout from '../components/MobileLayout'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <>
-      <MobileLayout>
-      </MobileLayout>
     </>
   )
 }
