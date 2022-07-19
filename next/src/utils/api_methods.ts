3700fac20b443e3d9f0974235cd3bc37a6847e13
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-07-20 01:34:56 +0900

    [add] api_methods.ts (#121)

---
+++ next/src/utils/api_methods.ts
@@ -0,0 +1,51 @@
+export const get = async (url: string) => {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return await res.json();
+  };
+  
+  export const get_with_token = async (url: string) => {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('access-token') || 'none',
+        client: localStorage.getItem('client') || 'none',
+        uid: localStorage.getItem('uid') || 'none',
+      },
+    });
+    return await res.json();
+  };
+  
+  export const post = async (url: string, data: any) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return res;
+  };
+  
+  export const put = async (url: string, data: any) => {
+    const res = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return res
+  };
+  
+  export const del = async (url: string) => {
+    const res = await fetch(url, { method: 'DELETE' });
+    return res;
+  };
+  
