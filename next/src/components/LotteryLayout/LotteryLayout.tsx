1369196d610a8b2a3663e21a1d4a1d929a40937b
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-08-08 22:00:28 +0900

    [fix] ちょっとだけ修正 (#126)

--- next/src/components/LotteryLayout/LotteryLayout.tsx
+++ next/src/components/LotteryLayout/LotteryLayout.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
-import ll from './LotteryLayout.module.css';
+import React from 'react'
+import ll from './LotteryLayout.module.css'
 
 interface LotteryLayout {
-  className?: string
-  align?: string
-  justify?: string
-  width?: string
-  height?: string
-  gap?: string
   children?: React.ReactNode
 }
 
 const LotteryLayout = (props: LotteryLayout) => {
   return (
     <div className={`${ll.OdometerContainer}`}>
-      <div className={`${ll.odometer}`}>
-        77777777
-      </div>
+      <div className={`${ll.odometer}`}>{props.children}</div>
     </div>
   )
 }
--- next/src/pages/admin/lottery.tsx
+++ next/src/pages/admin/lottery.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from 'next'
-import dynamic from 'next/dynamic'
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import LotteryLayout from '@/components/LotteryLayout'
-
+import Odometer from '@/components/Odometer'
 const Lottery: NextPage = () => {
   return (
-  <LotteryLayout/>
+    <LotteryLayout>
+      <Odometer value={'00000000'}></Odometer>
+    </LotteryLayout>
   )
 }
 
