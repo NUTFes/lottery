a28214b472ec575dd17a417a2b653a97a8d4d67a
ryuseifujisaki <fujiryu1122@gmail.com>
2022-08-12 00:07:13 +0900

    [issue138] CREATE mobilelayout

--- next/src/components/MobileLayout/MobileLayout.tsx
+++ next/src/components/MobileLayout/MobileLayout.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import ml from './MobileLayout.module.css';
+import React from 'react'
+import ml from './MobileLayout.module.css'
 interface MobileLayoutProps {
-  align?: string;
-  justify?: string;
-  width?: string;
-  height?: string;
-  gap?: string;
-  children?: React.ReactNode;
+  align?: string
+  justify?: string
+  width?: string
+  height?: string
+  gap?: string
+  children?: React.ReactNode
 }
 const MobileLayout = (props: MobileLayoutProps) => {
   return (
     <div className={`${ml.container}`}>
-      <img src="/bg_corner.png" className={`${ml.corner1}`} />
-      <img src="/bg_corner.png" className={`${ml.corner2}`} />
-      <img src="/bg_corner.png" className={`${ml.corner3}`} />
-      <img src="/bg_corner.png" className={`${ml.corner4}`} />
+      <div className={`${ml.odometerContainer}`}>
+        <img src="/bg_corner.png" className={`${ml.corner1}`} />
+        <img src="/bg_corner.png" className={`${ml.corner2}`} />
+        <img src="/bg_corner.png" className={`${ml.corner3}`} />
+        <img src="/bg_corner.png" className={`${ml.corner4}`} />
+        <img src="/side.png" className={`${ml.center3}`} />
+        <img src="/Lotelly.png" className={`${ml.center2}`} />
+        <br/> {/* 調整用改行*/}
+        <br/> {/* 調整用改行*/}
+        <br/> {/* 調整用改行*/}
+        <br/> {/* 調整用改行*/}
+        <img src="/side.png" className={`${ml.center3}`} />
+        <img src="/Results.png" className={`${ml.center1}`} />  
+        <table className={`${ml.design}`}>
+        <thead>
+          <tr>
+            <th className={`${ml.word1}`}>当選番号<hr className={`${ml.line}`} /></th>
+            <th className={`${ml.word2}`}>学籍番号<hr className={`${ml.line}`} /></th>
+          </tr>
+        </thead>
+        </table>
+        <img src="/side.png" className={`${ml.center4}`} />  
+      </div>
       {props.children}
     </div>
-  );
-};
-export default MobileLayout;
+  )
+  
+}
+export default MobileLayout
