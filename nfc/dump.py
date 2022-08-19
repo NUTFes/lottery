057413bbd26c8db90422aadbdc50ac9adcabe5c5
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-08-19 15:15:31 +0900

    [add] nfc docker setting

--- nfc/dump.py
+++ nfc/dump.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import binascii
 import os
 import sys
-sys.path.insert(1, os.path.split(sys.path[0])[0])
-import binascii
+
 import nfc
-service_code = 0x090f
+
+sys.path.insert(1, os.path.split(sys.path[0])[0])
+service_code = 0x090F
+
+
 def connected(tag):
-  # タグのIDなどを出力する
-  print (tag)
-  if isinstance(tag, nfc.tag.tt3.Type3Tag):
-    try:
-      # 内容を16進数で出力する
-      print('  ' + '\n  '.join(tag.dump()))
-    except Exception as e:
-      print ("error: %s" % e)
-  else:
-    print ("error: tag isn't Type3Tag")
+    # タグのIDなどを出力する
+    print(tag)
+    if isinstance(tag, nfc.tag.tt3.Type3Tag):
+        try:
+            # 内容を16進数で出力する
+            print("  " + "\n  ".join(tag.dump()))
+        except Exception as e:
+            print("error: %s" % e)
+    else:
+        print("error: tag isn't Type3Tag")
+
+
 # タッチ時のハンドラを設定して待機する
-clf = nfc.ContactlessFrontend('usb')
-clf.connect(rdwr={'on-connect': connected})
+clf = nfc.ContactlessFrontend("usb")
+clf.connect(rdwr={"on-connect": connected})
