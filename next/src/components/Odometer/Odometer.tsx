21e7bbc4477f98497be391a2d8b412d94ebbb823
Ryotakobayash <ryota5683804@gmail.com>
2022-08-29 12:18:49 +0900

    [mod]http://localhost:8888/test/odometerのフォント適応

--- next/src/components/Odometer/Odometer.tsx
+++ next/src/components/Odometer/Odometer.tsx
@@ -9,14 +9,16 @@ const Odometer = (props: OdometerProps) => {
     <div className={`${o.flex_container}`}>
       <div className={`${o.flex_container_gold}`}>
         <div className={`${o.flex_container_white}`}>
-          <Bounty
-            value={props.value}
-            initialValue={'77777777'}
-            lineHeight={1.35}
-            animationDelay={100}
-            duration={2000}
-            letterAnimationDelay={800}
-          />
+          <div className={`${o.lottery_font}`}>
+            <Bounty
+              value={props.value}
+              initialValue={'77777777'}
+              lineHeight={1.35}
+              animationDelay={100}
+              duration={2000}
+              letterAnimationDelay={800}
+            />
+          </div>
         </div>
       </div>
     </div>
