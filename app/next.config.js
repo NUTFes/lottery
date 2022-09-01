c6e86ac34f30190c9aa1cfa8774d78f1a00953c8
YushiroDodo63 <yushiro0603@gmail.com>
2022-09-01 23:27:34 +0900

    [add]basic Auth(#125)

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
+    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
+    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
+  }
 }
 
 module.exports = nextConfig
