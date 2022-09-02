4d757382066b4201df6fc9d0b0e563eaea6b5c0e
YushiroDodo63 <yushiro0603@gmail.com>
2022-09-02 23:08:50 +0900

    [add]post user(#132)

--- app/src/components/Button/index.ts
+++ app/src/components/Button/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button'
 export { default as ButtonNext } from './ButtonNext'
+export { default as AddButton } from './AddButton'
--- app/src/components/Toolbar/Toolbar.tsx
+++ app/src/components/Toolbar/Toolbar.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
-import { ButtonNext } from '@/components/Button'
+import { ButtonNext, AddButton } from '@/components/Button'
 import { post } from '@/utils/api_methods'
+ 
+interface UserData {
+  number: string
+  place_id: string
+}
+
 interface ToolbarProps {
   children?: React.ReactNode
   className?: string
 }
+ 
 const Toolbar = (props: ToolbarProps) => {
+  // For add user
+  const [userData, setUserData] = useState<UserData>({
+    number: '',
+    place_id: '',
+  })
+   
   const toDatetime = (date: Date) => {
+     
     return moment(date).toISOString(true).substr(0, 16)
   }
+  
+  // User handler
+  const userDataHandler =
+    (input: string) =>
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserData({ ...userData, [input]: e.target.value });
+      }
+  
+  // add user 
+  const postUser = async (data: UserData) => {
+    const postUrl = 'http://localhost:8000/api/user'
+    const postReq = await post(postUrl, data);
+    const postRes = await postReq.json();
+  }
+
   return (
     <aside aria-label="Toolbar" className={props.className}>
       <div className="mx-10">
@@ -38,7 +67,7 @@ const Toolbar = (props: ToolbarProps) => {
                 name="end_time"
                 defaultValue={toDatetime(new Date())}
                 id="end_time"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 dark:text-gray-100 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
                 placeholder=" "
                 required
               />
@@ -64,6 +93,8 @@ const Toolbar = (props: ToolbarProps) => {
                 id="student_id"
                 className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
                 placeholder=" "
+                value={userData.number}
+                onChange={userDataHandler('number')}
                 required
               />
               <label
@@ -80,6 +111,8 @@ const Toolbar = (props: ToolbarProps) => {
                 id="place_id"
                 className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
                 placeholder=" "
+                value={userData.place_id}
+                onChange={userDataHandler('place_id')}
                 required
               />
               <label
@@ -91,7 +124,9 @@ const Toolbar = (props: ToolbarProps) => {
             </div>
           </div>
           <div className="flex flex-row-reverse">
-            <ButtonNext href="#">追加</ButtonNext>
+            <AddButton onClick={() => {postUser(userData)}}>
+              追加
+            </AddButton>
           </div>
         </form>
         <form className="my-10">
