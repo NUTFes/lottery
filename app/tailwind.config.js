4e809a6e5b6ce77e0a189a0058ae8339b53804d8
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-09-07 20:47:20 +0900

    [fix] deployのための変更

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // 'media' or 'class',
   purge: {
-    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
     options: {
       // https://purgecss.com/safelisting.html#patterns
       safelist: {
