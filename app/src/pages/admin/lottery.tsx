0dc686ec0b2989d02f0bea61aef7c928d965c15a
YushiroDodo63 <yushiro0603@gmail.com>
2022-09-10 08:29:57 +0900

    [fix]remove delay logic

--- app/src/pages/admin/lottery.tsx
+++ app/src/pages/admin/lottery.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from 'next'
-import { useEffect, useRef, useState, useCallback } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import LotteryLayout from '@/components/LotteryLayout'
 import Odometer from '@/components/Odometer'
 import { get } from '@/utils/api_methods'
 
-type User = {
+interface User {
   id: number
   number: string
   updated_at: string
   created_at: String
 }
 
-type Props = {
+interface Props {
   users: User[]
 }
 
@@ -25,37 +25,16 @@ export const getServerSideProps = async () => {
   }
 }
 
-// 遅延を設定
-function useDelay(time: number) {
-  return useCallback(async () => {
-    return await new Promise((resolve) => {
-      setTimeout(resolve, time);
-    });
-  }, [time]);
-}
-
 const Lottery: NextPage<Props> = (props) => {
   const [users, setUsers] = useState<User[]>(props.users)
   const socketRef = useRef<WebSocket>()
   const [randomMessage, setRandomMessage] = useState('')
   
-  // 5秒後に当選者を取得するために遅延関数を定義
-  const delayed = useDelay(5000);
-   
-  const getWinner = useCallback(async () => {
-    try {
-      socketRef.current?.send(JSON.stringify({ client: 'Random', message: event.target[0].value }))
-      // 当選者を更新
-      delayed().then(() => {
-        setRandomMessage(users[0].number)
-      });
-    } catch (err) {
-      console.error(err)
-    }
-  }, [])
-
   useEffect(() => {
-    getWinner();
+    if(users[0].number) {
+      setRandomMessage(users[0].number)
+      socketRef.current?.send(JSON.stringify({ client: 'Random', message: users[0].number }))
+    }
      
     socketRef.current = new WebSocket(process.env.WS_API_URI)
 
