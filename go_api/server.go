c0eb134b8d27b2b8b2a0f5b3b45c95369a593867
hikahana <22.h.hanada.nutfes@gmail.com>
2022-10-13 23:35:09 +0900

    [mod]server.go内にswagger導入(#222)

--- go_api/server.go
+++ go_api/server.go
@@ -3,15 +3,45 @@ package main
 import (
   "github.com/labstack/echo/v4"
   _"gorm.io/gorm"
+
+  echoSwagger "github.com/swaggo/echo-swagger"
+
   "net/http"
+)
+
+type (
+  Response struct {
+      Int64  int64  `json:"int64"`
+      String string `json:"string"`
+      World  *Item  `json:"world"`
+  }
+
+  Item struct {
+      Text string `json:"text"`
+  }
 )
 
 func main() {
   e := echo.New()
   e.GET("/", healthCheck)
+  e.GET("/swagger/*", echoSwagger.WrapHandler)
+  e.GET("/checkswagger/", checkswagger)
   e.Logger.Fatal(e.Start(":1323"))
+  //e.Logger.Fatal(e.Start(":2313"))
 }
+
 
 func healthCheck(c echo.Context) error {
   return c.String(http.StatusOK, "Health Check")
 }
+
+func checkswagger(c echo.Context) error {
+  return c.JSON(http.StatusOK, &Response{
+      Int64:  1,
+      String: "string",
+      World: &Item{
+          Text: "checkswagger",
+      },
+  })
+}
+
