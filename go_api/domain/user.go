0f4e80d77edca78f3b58df79a08be3497f8f3bd8
hikahana <22.h.hanada.nutfes@gmail.com>
2022-10-29 16:10:23 +0900

    [mod]Numberの型をuintへ変更(#237)

--- go_api/domain/user.go
+++ go_api/domain/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	ID        uint      `json:"id" gorm:"primary_key"`
 	Name      string    `json:"name"`
-	Numeber   int       `json:"number" gorm:"unique;not null"`
+	Numeber   uint      `json:"number" gorm:"unique;not null"`
 	CreatedAT time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAT time.Time `json:"updated_at" gorm:"not null"`
 	Event     []Event   `gorm:"many2many:event_user;"`
