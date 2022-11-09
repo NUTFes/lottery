5d629c1a43917b7f08b1c1389f46f24bf1f409ae
hikahana <22.h.hanada.nutfes@gmail.com>
2022-11-10 00:23:18 +0900

    WIP adminのCRUD作成(#234)

---
+++ go_api/infrastructure/admin.go
@@ -0,0 +1,58 @@
+// DB操作
+package infrastructure
+
+import (
+	"github.com/NUTFes/lottery/go_api/domain"
+	"gorm.io/gorm"
+)
+
+type AdminInfrastructure struct {
+	db *gorm.DB
+}
+
+func NewAdminInfrastructure(db *gorm.DB) *AdminInfrastructure {
+	return &AdminInfrastructure{db: db}
+}
+
+// 全ユーザーの取得
+func (u *AdminInfrastructure) FindAll() (*domain.Admins, error) {
+	admins := domain.Admins{}
+	if err := u.db.Find(&admins).Error; err != nil {
+		return nil, err
+	}
+	return &admins, nil
+}
+
+// idを指定してユーザーを取得
+func (u *AdminInfrastructure) Find(id int) (*domain.Admin, error) {
+	admin := domain.Admin{}
+	if err := u.db.First(&admin, id).Error; err != nil {
+		return nil, err
+	}
+	return &admin, nil
+}
+
+// ユーザーの作成
+func (u *AdminInfrastructure) Create(admin *domain.Admin) error {
+	if err := u.db.Create(admin).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// ユーザーの更新
+func (u *AdminInfrastructure) Update(admin *domain.Admin) error {
+	if err := u.db.Updates(admin).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// ユーザーの削除
+func (u *AdminInfrastructure) Delete(id int) error {
+	admin := domain.Admin{}
+	if err := u.db.Delete(&admin, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
---
+++ go_api/interfaces/admin_controller.go
@@ -0,0 +1,91 @@
+// UserのControllerを定義する
+package controller
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/NUTFes/lottery/go_api/domain"
+	"github.com/NUTFes/lottery/go_api/usecase"
+	"github.com/labstack/echo/v4"
+)
+
+type adminController struct {
+	adminUsecase usecase.AdminUsecase
+}
+
+type AdminController interface {
+	IndexAdmin(c echo.Context) error
+	ShowAdmin(c echo.Context) error
+	CreateAdmin(c echo.Context) error
+	UpdateAdmin(c echo.Context) error
+	DeleteAdmin(c echo.Context) error
+}
+
+func NewAdminController(uu usecase.AdminUsecase) AdminController {
+	return &adminController{adminUsecase: uu}
+}
+
+// 全ユーザーの取得
+func (u *adminController) IndexAdmin(c echo.Context) error {
+	admins, err := u.adminUsecase.FindAllAdmin()
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, admins)
+}
+
+// idを指定してユーザーを取得
+func (u *adminController) ShowAdmin(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	admin, err := u.adminUsecase.FindAdmin(id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, admin)
+}
+
+// ユーザーの作成
+func (u *adminController) CreateAdmin(c echo.Context) error {
+	name := c.QueryParam("name")
+	email := c.QueryParam("email")
+
+	admin := &domain.Admin{
+		Name:      name,
+		Email:     email,
+		CreatedAT: time.Now(),
+		UpdatedAT: time.Now(),
+	}
+	if err := u.adminUsecase.CreateAdmin(admin); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, admin)
+}
+
+// ユーザーの更新
+func (u *adminController) UpdateAdmin(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	name := c.QueryParam("name")
+	email := c.QueryParam("email")
+
+	admin := &domain.Admin{
+		ID:        uint(id),
+		Name:      name,
+		Email:     email,
+		UpdatedAT: time.Now(),
+	}
+	if err := u.adminUsecase.UpdateAdmin(admin); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, admin)
+}
+
+// ユーザーの削除
+func (u *adminController) DeleteAdmin(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := u.adminUsecase.DeleteAdmin(id); err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, "Delete admin")
+}
