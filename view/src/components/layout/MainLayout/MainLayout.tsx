11b8c00772133f54ad5a20816cb54b2622982f4c
hikahana <22.h.hanada.nutfes@gmail.com>
2023-01-31 19:16:23 +0900

    [add]Footerのコンポーメント、レイアウトの作成(#287)

---
+++ view/src/components/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import Footer from '@components/common/Footer/Footer'
+
+const MainLayout: FC = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ view/src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainLayout from "@components/layout/MainLayout";
+
+const Home = () => {
+  return <MainLayout />;
+};
+
+export default Home;
