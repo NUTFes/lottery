efe62068c7eaaa6663618dbce9fc78f8b1932823
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-07 17:10:10 +0900

    [fix] local.envの環境変数をフロントで使えるようにするためにリネーム

--- view/src/pages/api/auth/[...nextauth].ts
+++ view/src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,9 @@ import KeycloakProvider from "next-auth/providers/keycloak";
 export default NextAuth({
   providers: [
     KeycloakProvider({
-        clientId: process.env.KEYCLOAK_ID!,
-        clientSecret: process.env.KEYCLOAK_SECRET!,
-        issuer: process.env.KEYCLOAK_ISSUER!,
+        clientId: process.env.NEXT_PUBLIC_KEYCLOAK_ID!,
+        clientSecret: process.env.NEXT_PUBLIC_KEYCLOAK_SECRET!,
+        issuer: process.env.NEXT_PUBLIC_KEYCLOAK_ISSUER!,
     }),
   ],
 });
