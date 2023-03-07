b472cecc197fc53d8b6172fb140d6db88b6980ef
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-07 17:08:42 +0900

    [feat] Headerにログアウトボタンを追加

--- view/src/components/common/Admin/AdminHeader/AdminHeader.tsx
+++ view/src/components/common/Admin/AdminHeader/AdminHeader.tsx
@@ -1,30 +1,30 @@
 import React, { FC } from 'react'
-import AppBar from '@mui/material/AppBar';
-import { createTheme } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar'
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import HeaderMenu from '@components/common/HeaderMenu'
 
 const theme = createTheme({
   palette: {
     primary: {
       main: '#0E1841',
     },
-  }
-});
+  },
+})
 
 const AdminHeader: FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position='static' color='primary' sx={{height: '64px'}}>
-      <Typography
-        fontSize='24px'
-        noWrap
-        component='div'
-        fontFamily='NOTO SANS JP'
-        sx={{ alignSelf: 'flex-start', marginLeft: '4%' , marginTop: '16px' }}
+      <AppBar
+        position="static"
+        color="primary"
+        sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '4rem', px: '2rem' }}
       >
-        管理者画面
-      </Typography>
+        <Typography fontSize="24px" noWrap component="div" fontFamily="NOTO SANS JP" sx={{ flexGrow: '1' }}>
+          管理者画面
+        </Typography>
+        <HeaderMenu mode="admin" />
       </AppBar>
     </ThemeProvider>
   )
